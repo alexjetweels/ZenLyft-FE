0301f4a58a3b23416857a8cf2938cdb86d32c150
Alex <alex@jetweels.com>
2024-08-24 15:21:03 +0700

    add: rank system

---
+++ src/enums/rank.ts
@@ -0,0 +1,17 @@
+export enum Rank {
+  SILVER = 'SILVER',
+  GOLD = 'GOLD',
+  PLATINUM = 'PLATINUM',
+  DIAMOND = 'DIAMOND',
+  MASTER = 'MASTER',
+  GRANDMASTER = 'GRANDMASTER',
+  CHALLENGER = 'CHALLENGER',
+}
+
+export enum SubRank {
+  RANK_1 = 'RANK_1',
+  RANK_2 = 'RANK_2',
+  RANK_3 = 'RANK_3',
+  RANK_4 = 'RANK_4',
+  RANK_5 = 'RANK_5',
+}
